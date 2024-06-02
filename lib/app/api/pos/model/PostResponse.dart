a4cc75a823cd96b070c9d6c7ae0e33a1c19800f7
jexpl12 <kolekmemilihmurtad@gmail.com>
2024-06-02 21:59:17 +0700

    api: [BW] api pos

---
+++ lib/app/api/pos/model/PostResponse.dart
@@ -0,0 +1,47 @@
+class PosResponse {
+  List<Data>? data;
+
+  PosResponse({this.data});
+
+  PosResponse.fromJson(Map<String, dynamic> json) {
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(new Data.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Data {
+  int? id;
+  String? name;
+  String? description;
+  String? image;
+
+  Data({this.id, this.name, this.description, this.image});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['description'] = this.description;
+    data['image'] = this.image;
+    return data;
+  }
+}
