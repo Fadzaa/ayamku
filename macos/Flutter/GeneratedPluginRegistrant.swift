bcd68f401661919f89dfb9fb61e5bda69b2489e7
Fattah Anggit Al Dzakwan <fa.anggit@gmail.com>
2026-09-11 16:05:03 +0700

    Migrate the Android build to the current Flutter toolchain
    Gradle 7.5 to 8.14, AGP 7.3 to 8.9, Kotlin to 2.2, Java 17, and a real release signing config. Pins dotted_border and pin_code_fields, and raises compileSdk to 36.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,20 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker_darwin
 import file_selector_macos
 import firebase_core
 import firebase_messaging
-import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
